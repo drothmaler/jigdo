8e0e1e0e40fabef3086fe7a5aafc9eddced82fcb
atterer <atterer>
2003-07-04 22:27:23 +0000

    Initial revision

---
+++ src/job/makeimage.cc
@@ -0,0 +1,21 @@
+/* $Id: makeimage.cc,v 1.1 2003-07-04 22:29:52 atterer Exp $ -*- C++ -*-
+  __   _
+  |_) /|  Copyright (C) 2003  |  richard@
+  | \/¯|  Richard Atterer     |  atterer.net
+  ¯ '` ¯
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2. See
+  the file COPYING for details.
+
+  Download & interpret .jigdo, download parts, assemble image
+
+*/
+
+#include <config.h>
+
+#include <glib.h>
+#include <iostream>
+#include <string.h>
+
+#include <makeimage.hh>
+//______________________________________________________________________
---
+++ src/util/nocopy.hh
@@ -0,0 +1,29 @@
+/* $Id: nocopy.hh,v 1.1 2003-07-04 22:30:12 atterer Exp $ -*- C++ -*-
+  __   _
+  |_) /|  Copyright (C) 2003  |  richard@
+  | \/¯|  Richard Atterer     |  atterer.net
+  ¯ '` ¯
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2. See
+  the file COPYING for details.
+
+  To be used as a base class only - prevents that the derived class can be
+  copied. It would be equivalent to add a private copy ctor and a private
+  assignment operator to the derived class, but deriving from NoCopy saves
+  typing and looks cleaner.
+
+*/
+
+#ifndef NOCOPY_HH
+#define NOCOPY_HH
+
+class NoCopy {
+protected:
+  NoCopy() { }
+  ~NoCopy() { }
+private:
+  NoCopy(const NoCopy&);
+  const NoCopy& operator=(const NoCopy&);
+};
+
+#endif
