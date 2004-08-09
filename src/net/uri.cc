148869dc1639ac3df17519244bf70aea13ec5b93
atterer <atterer>
2004-08-09 15:14:32 +0000

    *** empty log message ***

--- src/net/uri.cc
+++ src/net/uri.cc
@@ -1,7 +1,7 @@
-/* $Id: uri.cc,v 1.3 2004-08-09 14:57:21 atterer Exp $ -*- C++ -*-
+/* $Id: uri.cc,v 1.4 2004-08-09 15:14:32 atterer Exp $ -*- C++ -*-
   __   _
-  |_) /|  Copyright (C) 2001-2003  |  richard@
-  | \/¯|  Richard Atterer          |  atterer.net
+  |_) /|  Copyright (C) 2004  |  richard@
+  | \/¯|  Richard Atterer     |  atterer.net
   ¯ '` ¯
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2. See
